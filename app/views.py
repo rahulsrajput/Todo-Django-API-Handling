0b908fbd41f52435d29b26fc6cb9b8c9961a8015
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-02 11:38:46 +0530

    template part done

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def home(request):
+    return render(request, 'base/home.html')
--- todo/urls.py
+++ todo/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home')
 ]

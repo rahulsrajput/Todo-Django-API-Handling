afe9f9550fe7493cdd36ead73dd99ce00b7054c5
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-02 12:27:39 +0530

    api get(read) and post(create) done

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 import requests
+from django.http import HttpResponseRedirect
+import json
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    
+    url = 'https://api.freeapi.app/api/v1/todos'
+    
+    response = requests.get(url)
+    object = response.json()
+    # print(object)
+
+    if object['success'] and object['data']:
+        data = object['data']
+        
+        if request.method == 'POST':
+            title = request.POST['task']
+            new_data = {'title':title}
+            requests.post(url, new_data)
+            return HttpResponseRedirect('/')
+
+    
+    return render(request, 'base/home.html', context={'data':data})
